b27653eba655311a0cfa191983f53010648ab6af
Jovan <jovan.m.nixon@gmail.com>
2021-06-22 21:52:25 -0600

    deploying

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/whispering-badlands-35656", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/whispering-badlands", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   userCreateIndex: true,
